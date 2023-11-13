75afc6645df7d5655c5ef169af161126b81d49aa
HorroGlitch <lverot@edenschool.fr>
2023-11-13 16:29:52 +0100

    starting switch patern

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
+// Random switch
+
+// let switches = document.querySelectorAll("input");
+// console.log(switches);
+
+
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min) + min); 
+// }
+
+
+// for (let i = 0; i < switches.length; i++ ){
+//     switches[i].addEventListener("click", ()=>{
+//     let r = getRandomInt(0 ,switches.length);
+//     switches[r].checked = !switches[r].checked;
+//     });
+    
+    
+// }
+
+// console.log(getRandomInt, switches)
+
+
+// pattern switch
+
 let switches = document.querySelectorAll("input");
 console.log(switches);
 
+// function getPatternSwitch() {
+//     if (switches[0].checked){
+//         switches[3].checked;
+//         switches[2].checked;
+//     }
+// }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); 
-}
-
-
-for (let i = 0; i < switches.length; i++ ){
+for (let i = 0; i < switches.length; i++){
     switches[i].addEventListener("click", ()=>{
-    let r = getRandomInt(0 ,switches.length);
-    switches[r].checked = !switches[r].checked;
+        if (switches[0].checked) {
+            console.log("hi");
+        } else if (switches[1].checked){
+            console.log("howdy");
+        } 
     });
     
     
 }
-
-console.log(getRandomInt, switches)
 
